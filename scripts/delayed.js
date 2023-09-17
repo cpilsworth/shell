46a8e1bb3a0fe61c8919d9158f9222ffca914da8
Chris Pilsworth <chrisp@adobe.com>
2023-09-17 22:10:21 +0100

    universal editor

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+import './universal-editor-embedded.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
